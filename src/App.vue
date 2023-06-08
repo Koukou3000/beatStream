a6e61d68b64bdbf0a7d3ced8d32c13d854feb9ba
Koukou3000 <1968204044@qq.com>
2023-06-08 19:54:55 +0800

    boot

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      trend 近期试听热门，广告会有特殊标注 --discover
+      rank 销量 排行榜
+    </div>
+    <div>
+      详情页，专辑设置leading片段，在trend的swiper中展示
+
+
+      
+    </div>
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNav from './components/TopNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopNav
+  },
+  methods:{
+    dosth(){
+      console.log('123')
+    }
   }
 }
+
+
 </script>
 
 <style>
@@ -25,4 +43,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+
 </style>
---
+++ src/components/RecentTrend.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b>{{text}}</b>
+    
+  </div>
+</template>
+
+<script>
+export default {
+    props:['text']
+}
+</script>
+
+<style>
+
+</style>
